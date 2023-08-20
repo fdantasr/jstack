c1af81dce3352c5918a3e300c84b502d923bf9ba
Felipe Dantas <dantas.contatod@gmail.com>
2023-08-20 03:07:55 -0300

    Update index.js

--- NodeJs/FirstApi/index.js
+++ NodeJs/FirstApi/index.js
@@ -49,12 +49,10 @@ server.listen(3000, () =>
   console.log('🔥 Sever started at http://localhost:3000'),
 );
 
-// Emitted when the server starts listening
 server.addListener('request', (request, response) => {
   console.log('Request received');
 });
 
-// Emitted when the server closes
 server.addListener('close', () => {
   console.log('Server closed');
 });
