57486ce4f3e7b651b8cc5818308fc655de07861a
Felipe Dantas <dantas.contatod@gmail.com>
2023-08-19 23:38:52 -0300

    adding prettierrc

--- NodeJs/FirstApi/controllers/UserController.js
+++ NodeJs/FirstApi/controllers/UserController.js
@@ -12,40 +12,25 @@ const { sort } = require('../routes');
 
 module.exports = {
   listUsers(request, response) {
-
     const { order } = request.query;
 
     const sortedUsers = users.sort((a, b) => {
-      
       if (order === 'desc') {
         return a.id < b.id ? 1 : -1;
       }
 
       return a.id > b.id ? 1 : -1;
-
     });
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(
-      JSON.stringify(sortedUsers)
-    )
+    response.snd(200, sortedUsers);
   },
   getUserById(request, response) {
-
     const { id } = request.params;
     const user = users.find((user) => user.id === Number(id));
 
     if (!user) {
-      response.writeHead(400, { 'Content-Type': 'application/json' });
-      response.end(
-        JSON.stringify({ error: 'User not found' })
-      )
-      
+      return response.send(400, { error: 'User not found' });
+    } else {
+      response.send(200, user);
     }
-    else {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.end(
-        JSON.stringify({ user }))
-    }
-   
-  }
+  },
 };
--- NodeJs/FirstApi/index.js
+++ NodeJs/FirstApi/index.js
@@ -1,43 +1,48 @@
 const http = require('http');
-const { URL } =  require('url');
+const { URL } = require('url');
 const routes = require('./routes');
 
-// Create a server object with an anonymous function
 const server = http.createServer((request, response) => {
-  const parsedUrl = new URL(`http://localhost:3000${request.url}`); //Cria uma nova instância de URL com o endereço da requisição
-  console.log(`Request method:, ${request.method} | Endpoint: ${parsedUrl.pathname}}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(
+    `Request method:, ${request.method} | Endpoint: ${parsedUrl.pathname}}`,
+  );
 
-  let { pathname } = parsedUrl; //Desestruturação do pathname
+  let { pathname } = parsedUrl;
   let id = null;
 
-  const splitEndpoint = pathname.split('/').filter(Boolean); //Separa o pathname em um array e remove os espaços vazios
+  const splitEndpoint = pathname.split('/').filter(Boolean);
   console.log(splitEndpoint);
 
   if (splitEndpoint.length > 1) {
-  pathname = `/${splitEndpoint[0]}/:id`; //Cria um novo pathname com o endpoint e o id
-  id = splitEndpoint[1]; //Atribui o id
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
   }
 
-  //Recebe a rota que está iterando passando o endpoint e o método
-  const route = routes.find((routeObj) => (
- 
-    routeObj.endpoint === pathname && routeObj.method === request.method
-  ));
+  const route = routes.find(
+    (routeObj) =>
+      routeObj.endpoint === pathname && routeObj.method === request.method,
+  );
 
   if (route) {
-    request.query = Object.fromEntries(parsedUrl.searchParams); //Transforma o objeto em um array de arrays e depois em um objeto novamente
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     request.params = { id };
 
-    route.handler(request, response);
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body));
+    };
 
+    route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
-
 });
 
-server.listen(3000, () => console.log('🔥 Sever started at http://localhost:3000'))
+server.listen(3000, () =>
+  console.log('🔥 Sever started at http://localhost:3000'),
+);
 
 // Emitted when the server starts listening
 server.addListener('request', (request, response) => {
